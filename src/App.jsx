7cf529283e28608f7ef9aec0cd6110ec2e18023e
TheCoderArif <mdarifulislam18539@gmail.com>
2025-12-09 05:56:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='px-10'>
+
+        <h1>Welcome to the cricket world.</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
